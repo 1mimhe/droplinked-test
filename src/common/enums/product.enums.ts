adb4a49d5d378c1868e207642b58f80bc94077b1
Mohammad Hosseini <mimhe1381@gmail.com>
2025-12-06 13:17:15 +0330

    Add product enum

---
+++ src/common/enums/product.enums.ts
@@ -0,0 +1,21 @@
+export enum ProductType {
+  DIGITAL = 'digital',
+  PHYSICAL = 'physical',
+}
+
+export enum ProductStatus {
+  DRAFT = 'draft',
+  PUBLISHED = 'published',
+}
+
+export enum ProductVisibility {
+  PUBLIC = 'public',
+  PRIVATE = 'private',
+}
+
+export enum ShippingMethod {
+  STANDARD = 'standard',
+  EXPRESS = 'express',
+  OVERNIGHT = 'overnight',
+  INTERNATIONAL = 'international',
+}
