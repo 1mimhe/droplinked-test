330d43a19ac5b519c774a603a63d533e1bd5f610
Mohammad Hosseini <mimhe1381@gmail.com>
2025-12-06 10:07:51 +0330

    Add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Droplinked - Product Service')
+    .setDescription('A test project')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, documentFactory);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  const PORT = process.env.PORT ?? 3000;
+  await app.listen(PORT, '0.0.0.0', () => console.log(`Listening on port ${PORT}.`));
 }
 bootstrap();
